1a0b3fb9b7c62c5fc608d01a56fe504fa3192f0d
daisnus <nikita.kozlov.200516@gmail.com>
2026-06-01 10:47:21 +0500

    Реализовал UnitOfWork паттерн с использованием CAP библиотеки для реализации Outbox Transactional паттерна. Удалил ненужную миграцию.

---
+++ backend/ProductService/ProductService.Infrastructure/InfrastructureExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ProductService.Infrastructure;
+
+public static class InfrastructureExtensions
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        throw new NotImplementedException();
+    }
+}

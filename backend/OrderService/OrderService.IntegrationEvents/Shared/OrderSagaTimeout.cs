01f859e29a9ad37c4046bf5f6c2c8336c38118b1
Golubaya <35600056+Goluboy@users.noreply.github.com>
2026-06-02 06:22:53 +0500

    add timeout

---
+++ backend/OrderService/OrderService.IntegrationEvents/Shared/OrderSagaTimeout.cs
@@ -0,0 +1,6 @@
+﻿namespace OrderService.IntegrationEvents.Shared;
+
+public record OrderSagaTimeout
+{
+    public Guid CorrelationId { get; init; } // = OrderId
+}
---
+++ backend/ProductService/ProductService.IntegrationEvents/Shared/OrderSagaTimeout.cs
@@ -0,0 +1,6 @@
+﻿namespace ProductService.IntegrationEvents.Shared;
+
+public record OrderSagaTimeout
+{
+    public Guid CorrelationId { get; init; } // = OrderId
+}

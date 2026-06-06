ec9f87ee87f5f80ecb011502c0751146132dd770
daisnus <nikita.kozlov.200516@gmail.com>
2026-06-06 12:56:50 +0500

    Зарегистрировал сервисы в DI.

--- backend/ProductService/ProductService.Infrastructure/InfrastructureExtensions.cs
+++ backend/ProductService/ProductService.Infrastructure/InfrastructureExtensions.cs
@@ -1,8 +1,15 @@
 ﻿using Dapper;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using ProductService.Infrastructure.Abstractions.Repository.Abstractions;
+using ProductService.Infrastructure.Abstractions.Repository.Abstractions.Products;
+using ProductService.Infrastructure.Abstractions.UnitOfWork.Abstractions;
 using ProductService.Infrastructure.DAO;
 using ProductService.Infrastructure.Helpers.JsonbSerialization;
+using ProductService.Infrastructure.Persistence.Provider;
+using ProductService.Infrastructure.Repository;
+using ProductService.Infrastructure.Repository.Products;
+using ProductService.Infrastructure.UnitOfWork;
 
 namespace ProductService.Infrastructure;
 
@@ -11,6 +18,17 @@ public static class InfrastructureExtensions
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         SqlMapper.AddTypeHandler(new JsonbTypeHandler<List<ProductImageDao>>());
+
+        services.AddSingleton<IPostgresConnectionFactory, PostgresConnectionFactory>();
+
+        services.AddScoped<UnitOfWork.UnitOfWork>();
+        
+        services.AddScoped<IDbSession>(sp => sp.GetRequiredService<UnitOfWork.UnitOfWork>());
+        services.AddScoped<IUnitOfWork>(sp => sp.GetRequiredService<UnitOfWork.UnitOfWork>());
+        
+        services.AddScoped<ICategoryRepository, CategoryRepository>();
+        services.AddScoped<IProductQueryRepository, ProductQueryRepository>();
+        services.AddScoped<IProductRepository, ProductRepository>();
         
         return services;
     }

3dcccdef63f2e20e2e458224a2b3eb92f19401f7
daisnus <nikita.kozlov.200516@gmail.com>
2026-06-08 18:47:52 +0500

    Регистрация в DI.

--- backend/ProductService/ProductService.Application/ApplicationExtensions.cs
+++ backend/ProductService/ProductService.Application/ApplicationExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using ProductService.Application.Services.Categories;
 using ProductService.Application.Services.Products;
+using ProductService.Application.Services.Products.Command;
 using ProductService.Application.Services.Products.Query;
 using ProductService.Infrastructure.Abstractions.Repository.Abstractions.Products;
 
@@ -12,6 +13,7 @@ public static class ApplicationExtensions
     {
         services.AddScoped<ICategoryService, CategoryService>();
         services.AddScoped<IProductQueryService, ProductQueryService>();
+        services.AddScoped<IProductCommandService, ProductCommandService>();
         
         return services;
     }

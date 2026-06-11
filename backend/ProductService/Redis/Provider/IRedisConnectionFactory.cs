26c832a9f9843f7f9cd21a84919dd51b65c4371e
daisnus <nikita.kozlov.200516@gmail.com>
2026-06-11 19:56:23 +0500

    Реализовал библиотеку для кеширования в Redis с поддержкой MGET и MSET. Клиент для кеширование поддерживает настройку конфигурации с помощью RedisOptions

---
+++ backend/ProductService/Redis/Provider/IRedisConnectionFactory.cs
@@ -0,0 +1,8 @@
+﻿using StackExchange.Redis;
+
+namespace Redis.Provider;
+
+internal interface IRedisConnectionFactory
+{
+    IDatabase GetDatabase();
+}
---
+++ backend/ProductService/Redis/Service/ICacheService.cs
@@ -0,0 +1,13 @@
+﻿namespace Redis.Service;
+
+public interface ICacheService
+{
+    Task<T?> GetAsync<T>(string key, CancellationToken ct = default);
+    Task<IReadOnlyList<T?>> GetManyAsync<T>(IReadOnlyList<string> keys, CancellationToken ct = default);
+    
+    Task SetAsync<T>(string key, T value, TimeSpan? expiry = null, CancellationToken ct = default);
+    Task SetManyAsync<T>(IReadOnlyDictionary<string, T> values, TimeSpan? expiry = null, CancellationToken ct = default);
+    
+    Task RemoveAsync(string key, CancellationToken ct = default);
+    Task RemoveManyAsync(IEnumerable<string> keys, CancellationToken ct = default);
+}

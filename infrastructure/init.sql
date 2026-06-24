7628f959afacebdf36c17c86d06c03a3a6a0e2f7
daisnus <nikita.kozlov.200516@gmail.com>
2026-06-24 15:27:47 +0500

    Fix

--- infrastructure/init.sql
+++ infrastructure/init.sql
@@ -1,3 +1,3 @@
-﻿CREATE DATABASE products;
-CREATE DATABASE orders;
-CREATE DATABASE keycloak;
+﻿SELECT 'CREATE DATABASE products' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'products')\gexec
+SELECT 'CREATE DATABASE orders' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'orders')\gexec
+SELECT 'CREATE DATABASE keycloak' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'keycloak')\gexec
